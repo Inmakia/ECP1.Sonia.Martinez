12e1d51c492b4c83ec4874d8439f2434f7402048
SoniaMM <inmakia@gmail.com>
2014-10-01 10:06:02 +0200

    Añadido método multiplicarInts

--- src/main/java/upm/miw/spai/ecp1/Operaciones.java
+++ src/main/java/upm/miw/spai/ecp1/Operaciones.java
@@ -32,6 +32,10 @@ public class Operaciones {
     public int multiplicarInts(int int1, int int2) {
         return int1 * int2;
     }
+    
+    public float multiplicarFloats(float float1, float float2){
+        return float1 * float2;
+    }
 
     public int dividirInts(int int1, int int2) {
         return (int) (int1 / int2);
