06849ed5c06c5fb19003070d7049dd70f21d7424
SoniaMM <inmakia@gmail.com>
2014-10-01 09:27:33 +0200

    Añadidos atributos enteros de clase Operaciones y sus getters y setters

--- src/main/java/upm/miw/spai/ecp1/Operaciones.java
+++ src/main/java/upm/miw/spai/ecp1/Operaciones.java
@@ -1,5 +1,31 @@
 package upm.miw.spai.ecp1;
 
 public class Operaciones {
+    
+    private int int1, int2, int3;
 
+    public int getInt1() {
+        return int1;
+    }
+
+    public void setInt1(int int1) {
+        this.int1 = int1;
+    }
+
+    public int getInt2() {
+        return int2;
+    }
+
+    public void setInt2(int int2) {
+        this.int2 = int2;
+    }
+
+    public int getInt3() {
+        return int3;
+    }
+
+    public void setInt3(int int3) {
+        this.int3 = int3;
+    }
+    
 }
